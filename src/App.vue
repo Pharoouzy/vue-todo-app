59d12088cdf73f170c75ecb2d0a92b07ed4e97e7
Pharoouzy <pharoouzy@yahoo.com>
2020-06-16 21:33:45 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,11 +9,26 @@
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What needs to be done"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+    />
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      newTodo: "",
+      idForTodo: 3,
+      todos: [
+        {
+          id: 1,
+          title: "Finish Vue Course",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Take Over the world",
+          completed: false
+        }
+      ]
+    };
+  },
+  methods: {
+    addTodo(){
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      });
+
+      this.newTodo = "";
+      this.idForTodo++;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+
+  &:focus {
+    outline: 0;
+  }
+}
+</style>
